a406ac23cce4fb5da422cc10d3ea3ad5592dedf9
pachill <pachill289@gmail.com>
2026-04-15 22:32:18 -0400

    ver 0.2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, Request
 import fitz  # PyMuPDF
 
 app = FastAPI()
@@ -8,15 +8,14 @@ def home():
     return {"message": "API PDF Tigo funcionando"}
 
 @app.post("/extract-text")
-async def extract_text(file: UploadFile = File(...)):
-    content = await file.read()
-
-    doc = fitz.open(stream=content, filetype="pdf")
-
+async def extract_text(request: Request):
+    # Leer el body crudo como bytes
+    pdf_bytes = await request.body()
+    
+    doc = fitz.open(stream=pdf_bytes, filetype="pdf")
+    
     text = ""
     for page in doc:
         text += page.get_text()
-
-    return {
-        "text": text
-    }
+    
+    return {"text": text}
